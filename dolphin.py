f93cbcee9ce8b9e0fad8fad493c0210e238ec202
Felipe Trujillo <felipe.trujillo@valorunico.cl>
2024-01-22 14:26:30 -0300

    e

--- dolphin.py
+++ dolphin.py
@@ -1,46 +1,21 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_name_or_path = "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ"
-# To use a different branch, change revision
-# For example: revision="gptq-4bit-32g-actorder_True"
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path,
-                                             device_map="auto",
-                                             trust_remote_code=False,
-                                             revision="main")
+device = "cuda" # the device to load the model onto
 
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
 
-system_prompt = """Eres un asistente útil llamado Dayana, utilizarás el contexto proporcionado para responder preguntas de los usuarios. 
-Lee el contexto dado antes de responder preguntas y piensa paso a paso. 
-Si no puedes responder una pregunta del usuario basándote en el contexto proporcionado, 
-informa al usuario. No utilices ninguna otra información para responder al usuario. Proporciona una respuesta detallada a la pregunta. Responde siempre en español."""
+messages = [
+    {"role": "user", "content": "What is your favourite condiment?"},
+    {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
+    {"role": "user", "content": "Do you have mayonnaise recipes?"}
+]
 
+encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
 
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+model_inputs = encodeds.to(device)
+model.to(device)
 
-prompt = input("\nEnter a query: ")
-
-prompt_template=f'''<s>[INST]{system_prompt} {prompt} [/INST]
-'''
-
-print("\n\n*** Generate:")
-
-input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
-output = model.generate(inputs=input_ids, temperature=0.7, do_sample=True, top_p=0.95, top_k=40, max_new_tokens=512)
-#print(tokenizer.decode(output[0]))
-
-# Inference can also be done using transformers' pipeline
-
-print("*** Pipeline:")
-pipe = pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    max_new_tokens=512,
-    do_sample=True,
-    temperature=0.7,
-    top_p=0.95,
-    top_k=40,
-    repetition_penalty=1.1
-)
-
-print(pipe(prompt_template)[0]['generated_text'])
+generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+decoded = tokenizer.batch_decode(generated_ids)
+print(decoded[0])
