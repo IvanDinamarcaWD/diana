7385a01a63c83ab62bb5b36ad05b9293a6683b48
Felipe Trujillo <felipe.trujillo@valorunico.cl>
2024-01-22 16:21:05 -0300

    e

--- dayanastream.py
+++ dayanastream.py
@@ -1,19 +1,5 @@
-from prompt_template_utils import get_prompt_template
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 from transformers import pipeline
-from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.llms import HuggingFacePipeline
-from langchain.chains import RetrievalQA
-
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler  # for streaming response
-from langchain.callbacks.manager import CallbackManager
-from constants import (
-    EMBEDDING_MODEL_NAME,
-    PERSIST_DIRECTORY,
-    CHROMA_SETTINGS
-)
-callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
 model_name_or_path = "TheBloke/Mistral-7B-Instruct-v0.2-AWQ"
 
@@ -23,47 +9,19 @@ model = AutoModelForCausalLM.from_pretrained(
     low_cpu_mem_usage=True,
     device_map="cuda:0"
 )
-prompt, memory = get_prompt_template(promptTemplate_type="llama", history=False)
 
 # Using the text streamer to stream output one token at a time
 streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
-embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": "cuda"})
+prompt = "Tell me about AI"
+prompt_template=f'''<s>[INST] {prompt} [/INST]
+'''
 
-db = Chroma(
-    persist_directory=PERSIST_DIRECTORY,
-    embedding_function=embeddings,
-    client_settings=CHROMA_SETTINGS
-)
-retriever = db.as_retriever()
-
-pipe = pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    max_new_tokens=4096,
-    do_sample=True,
-    temperature=0.7,
-    top_p=0.95,
-    top_k=40,
-    repetition_penalty=1.1
-)
-prompt, memory = get_prompt_template(promptTemplate_type="llama", history=False)
-
-llm = HuggingFacePipeline(pipeline=pipe)
-
-qa = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff",  # try other chains types as well. refine, map_reduce, map_rerank
-    retriever=retriever,
-    return_source_documents=True,  
-    #verbose=True,
-    #streaming: True,
-    callbacks=callback_manager,
-    chain_type_kwargs={
-        "prompt": prompt,
-    },
-)
+# Convert prompt to tokens
+tokens = tokenizer(
+    prompt_template,
+    return_tensors='pt'
+).input_ids.cuda()
 
 generation_params = {
     "do_sample": True,
@@ -74,30 +32,32 @@ generation_params = {
     "repetition_penalty": 1.1
 }
 
+# Generate streamed output, visible one token at a time
+generation_output = model.generate(
+    tokens,
+    streamer=streamer,
+    **generation_params
+)
 
+# Generation without a streamer, which will include the prompt in the output
+generation_output = model.generate(
+    tokens,
+    **generation_params
+)
 
-while True:
-    query = input("\nEnter a query: ")
-    res = qa(query)
-
-    # Convert prompt to tokens
-    tokens = tokenizer(
-        query,
-        return_tensors='pt'
-    ).input_ids.cuda()
-
-
-    # Generate streamed output, visible one token at a time
-    generation_output = model.generate(
-        tokens,
-        streamer=streamer,
-        **generation_params
-    )
-    # Get the tokens from the output, decode them, print them
-    token_output = generation_output[0]
-    text_output = tokenizer.decode(token_output)
-    print("model.generate output: ", text_output)
+# Get the tokens from the output, decode them, print them
+token_output = generation_output[0]
+text_output = tokenizer.decode(token_output)
+print("model.generate output: ", text_output)
 
 # Inference is also possible via Transformers' pipeline
 
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    **generation_params
+)
 
+pipe_output = pipe(prompt_template)[0]['generated_text']
+print("pipeline output: ", pipe_output)
