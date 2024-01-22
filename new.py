c8cbe4d16b56c8fe8713977504f45c5cdc0d6733
Felipe Trujillo <felipe.trujillo@valorunico.cl>
2024-01-22 16:28:07 -0300

    e

---
+++ new.py
@@ -0,0 +1,24 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+
+from constants import (
+    EMBEDDING_MODEL_NAME,
+    PERSIST_DIRECTORY,
+    CHROMA_SETTINGS
+)
+
+
+embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": "cuda"})
+# uncomment the following line if you used HuggingFaceEmbeddings in the ingest.py
+# embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
+
+# load the vectorstore
+db = Chroma(
+    persist_directory=PERSIST_DIRECTORY,
+    embedding_function=embeddings,
+    client_settings=CHROMA_SETTINGS
+)
+retriever = db.as_retriever()
+
+print(retriever)
