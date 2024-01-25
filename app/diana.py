0e076baae374b92be54e2bb39a604d2a5ae644ef
Felipe Trujillo <felipe.trujillo@valorunico.cl>
2024-01-25 11:16:31 -0300

    e

--- app/diana.py
+++ app/diana.py
@@ -124,5 +124,7 @@ def newPrompt(user_question: str):
     qa_chain_response = qa.stream(
         {"query": user_question},
     )
-    yield qa_chain_response
+
+    return qa_chain_response["result"]
+    #yield qa_chain_response
    
