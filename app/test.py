1d982442d26de6293ffb1f9a441a40717ccb1f5a
Felipe Trujillo <felipe.trujillo@valorunico.cl>
2024-01-25 11:23:49 -0300

    e

--- app/test.py
+++ app/test.py
@@ -1,7 +1,7 @@
 import random
 from typing import Dict, Any, Iterator
 from diana import newPrompt
-
+import asyncio
 def generate_random_data() -> Iterator[Dict[str, Any]]:
     while True:
         yield {
@@ -11,9 +11,21 @@ def generate_random_data() -> Iterator[Dict[str, Any]]:
             # Add more keys and random value generation as needed
         }
 
+async def _generate(answer: str):
+	chunk_size = 10
+	num_chunks = (len(answer) + chunk_size - 1) // chunk_size
+	for i in range(num_chunks):
+		chunk_start = i * chunk_size
+		chunk_end = (i + 1) * chunk_size
+		chunk = answer[chunk_start:chunk_end]
+		response_body = f"{chunk}"
+		yield response_body.encode()
+		await asyncio.sleep(0.1)
+
 # Example usage:
 if __name__ == "__main__":
     data_generator = newPrompt('hola, responde en dos palabras')
 
-    for i in data_generator:
-        print(next(i))
+    _generate(data_generator)
+    #for i in data_generator:
+    #    print(next(i))
