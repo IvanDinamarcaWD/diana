4065a1d958584fc0723e835c2e12896b7bb854a5
Felipe Trujillo <felipe.trujillo@valorunico.cl>
2024-01-25 10:49:09 -0300

    e

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException,streaming
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from diana import newPrompt
@@ -31,7 +31,7 @@ def prompt(data: dict):
         async def data_generator_func():
             for item in data_generator:
                 # Yield each item serialized as JSON
-                yield streaming.StringResponse(content=item)
+                yield item
                 
         #def generate():
         #    for _ in range(5):  # Simulate 5 chunks of fake data
