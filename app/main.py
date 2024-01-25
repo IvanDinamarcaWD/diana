64dacc10bada81314287786a8d5977ec1a824308
Felipe Trujillo <felipe.trujillo@valorunico.cl>
2024-01-25 11:10:26 -0300

    e

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from diana import newPrompt
 from typing import Dict, Iterator
 
-#import time
+import time
 app = FastAPI()
 
 # Configure CORS middleware
@@ -25,14 +25,15 @@ def prompt(data: dict):
             data_generator = newPrompt(question)
             for item in data_generator:
                 
+                print(next(item))
                 # Yield each item serialized as JSON
-                yield next(item['result'])
+                #yield next(item['result'])
                 
-        #def generate():
-        #    for _ in range(5):  # Simulate 5 chunks of fake data
-        #        time.sleep(1)  # Simulate some processing time
-        #        yield f"Received message: {message}\n".encode("utf-8")
-        return StreamingResponse(data_generator_func())
+        def generate():
+            for _ in range(5):  # Simulate 5 chunks of fake data
+                time.sleep(1)  # Simulate some processing time
+                yield f"Received message: {question}\n".encode("utf-8")
+        return StreamingResponse(generate())
     
     except KeyError:
         raise HTTPException(status_code=400, detail="Message not provided in request.")
